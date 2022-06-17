13de08d12ae47f4486a6e78f1c1f89b2b8fce401
bhavyapatel2002 <bhavyapatel2002@gmail.com>
2022-06-17 16:38:44 -0400

    added content

--- app.py
+++ app.py
@@ -1,10 +1,55 @@
-from flask import Flask
+from crypt import methods
+from flask import Flask, redirect, url_for, render_template, request
+from rainwater import trap_rain_water
+from rainwater import plot_rain_water
+import random
+import os
 
+# initialize Flask application
 app = Flask(__name__)
 
-@app.route('/')
+# landing page
+@app.route("/")
 def home():
-    return "Flask heroku app.TESTTTTT"
+    return render_template("home.html")
 
-if __name__ == '__main__':
-    app.run()
+# this page is never directly accessed, it is embedded into trapping rain water page as IFrame
+@app.route("/rain-water-model")
+def rain_water_model():
+    return render_template("rainwatermodel.html")
+
+
+@app.route("/trapping-rain-water/", methods=['GET', 'POST'])
+def trapping_rain_water():
+    # randomly generate first time
+    int_list = [random.randint(0, 10) for i in range(12)]
+    
+    temp = trap_rain_water(int_list)
+    water = temp[0]
+    trapped = temp[1]
+
+    # all subsequent times, only randomly generate if client does not enter a list
+    if request.method == 'POST':
+        temp = request.form['list']
+
+        if not temp:
+            int_list = [random.randint(0, 10) for i in range(12)]
+        else:
+            # split user input into list of integers
+            int_list = [int(x) for x in temp.split(',') if x.strip().isdigit()]
+
+        temp = trap_rain_water(int_list)
+        water = temp[0]
+        trapped = temp[1]
+
+    # generate Plotly model and export to HTML file
+    plot_rain_water(int_list, trapped)
+    
+    # render template and pass in trapped water value
+    return render_template("trappingrainwater.html", waterValue=water)
+
+
+if __name__ == "__main__":
+    # launch Flask app
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
---
+++ rainwater.py
@@ -0,0 +1,64 @@
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+import chart_studio.tools as tls
+
+
+def trap_rain_water(heights):
+    # two pointer approach
+    left = 0
+    right = len(heights) - 1
+    trapped = [0]*len(heights)
+
+    maxLeft = heights[left]
+    maxRight = heights[right]
+
+    water = 0
+
+    while left < right:
+        if maxLeft < maxRight:
+            left += 1
+
+            if heights[left] < maxLeft:
+                water += (maxLeft - heights[left])
+                trapped[left] = maxLeft - heights[left]
+            else:
+                maxLeft = heights[left]
+        else:
+            right -= 1
+
+            if heights[right] < maxRight:
+                water += (maxRight - heights[right])
+                trapped[right] = maxRight - heights[right]
+            else:
+                maxRight = heights[right]
+
+    return water, trapped
+
+# generate Plotly model
+def plot_rain_water(heights, trapped):
+    fig = go.Figure(data=[
+        go.Bar(name='Bars', y=heights, marker_color='rgb(145, 150, 158)'),
+        go.Bar(name='Water', y=trapped, marker_color='rgb(162, 228, 250)')
+    ])
+
+    fig.update_layout(
+        title='Rain Water Model',
+        xaxis_title='Index',
+        yaxis_title='Rain Water Collected',
+        barmode='stack',
+        bargap=0,
+        margin=dict(
+            l=50,
+            r=50,
+            b=70,
+            t=15,
+            pad=4
+        ),
+    )
+    fig.update_yaxes(dtick='1')
+    fig.update_xaxes(dtick='1')
+    fig.layout.title = None
+
+    # export to HTML file, which can later be embedded as IFrame
+    fig.write_html("./templates/rainwatermodel.html")
